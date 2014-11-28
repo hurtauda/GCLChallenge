580196aa41a700b2e2858423647625163d87413b
hurtauda <alban.hurtaud@hotmail.fr>
2014-11-28 13:37:52 +0100

    commit de la mort

--- upload.php
+++ upload.php
@@ -3,12 +3,15 @@ if(isset($_FILES['image'])) {
     $dossier = 'upload/';
     $fichier = basename($_FILES['image']['name']);
 
-    preg_match("/^.+\\.(.+)$/", $fichier, $matches);
+    preg_match("/^(.+)\\.(.+)$/", $fichier, $matches);
 
-    if ((strtolower($matches[1]) === "jpg") || (strtolower($matches[1]) === "jpeg") || (strtolower($matches[1]) === "png")) {
+    if ((strtolower($matches[2]) === "jpg") || (strtolower($matches[2]) === "jpeg") || (strtolower($matches[2]) === "png")) {
 
         if (move_uploaded_file($_FILES['image']['tmp_name'], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
         {
+            file_put_contents('img/d/user/'.$matches[1].'.prop', $_POST['description']);
+            file_put_contents('img/m/user/'.$matches[1].'.prop', $_POST['description']);
+
             $result = exec('/bin/sh scripts/scriptUpload.sh ' . $dossier . $fichier . ' ' . $fichier);
             header('Location: index.html');
         } else //Sinon (la fonction renvoie FALSE).
