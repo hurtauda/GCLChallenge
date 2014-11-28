673f263028c65771fb6c94515c628d83bbf94efb
hurtauda <alban.hurtaud@hotmail.fr>
2014-11-28 10:19:09 +0100

    modification script server4

--- scripts/scriptRefreshServerImages.sh
+++ scripts/scriptRefreshServerImages.sh
@@ -11,17 +11,17 @@ rm img/m/serv/*
 for file in /var/www/gclcimages/*; do
 #for file in /Users/hurtauda/GCLChallenge/GCLChallenge/gclcimages/*; do
         filename=$(basename "$file")
-        extension="${filename##*.}"
-        filenamenoextension="${filename%.*}"
-        jpgextension=".jpg"
+        #extension="${filename##*.}"
+        #filenamenoextension="${filename%.*}"
+        #jpgextension=".jpg"
 
-        if [[ $extension == "png" ]]
-        then
-            /usr/bin/mogrify -path upload -format jpg "${file}"
-    	    /usr/bin/convert upload/"${filenamenoextension}"$jpgextension -scale 900x360\> img/d/serv/"${filenamenoextension}"$jpgextension
-    	    /usr/bin/convert upload/"${filenamenoextension}"$jpgextension -scale 320x128\> img/m/serv/"${filenamenoextension}"$jpgextension
-    	    rm upload/"${filenamenoextension}"$jpgextension
-    	elif [[ $extension == "jpg" ]] || [[ $extension == "jpeg" ]]
+        #if [[ $extension == "png" ]]
+        #then
+        #    /usr/bin/mogrify -path upload -format jpg "${file}"
+    	#    /usr/bin/convert upload/"${filenamenoextension}"$jpgextension -scale 900x360\> img/d/serv/"${filenamenoextension}"$jpgextension
+    	#    /usr/bin/convert upload/"${filenamenoextension}"$jpgextension -scale 320x128\> img/m/serv/"${filenamenoextension}"$jpgextension
+    	#    rm upload/"${filenamenoextension}"$jpgextension
+    	if [[ $extension == "png" ]] || [[ $extension == "jpg" ]] || [[ $extension == "jpeg" ]]
     	then
     	    /usr/bin/convert $file -scale 900x360\> img/d/serv/$filename
     	    /usr/bin/convert $file -scale 320x128\> img/m/serv/$filename
