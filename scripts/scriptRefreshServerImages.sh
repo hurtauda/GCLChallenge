4bc358911899f6e3b775923875cc35a6b7940fc9
hurtauda <alban.hurtaud@hotmail.fr>
2014-11-28 10:08:43 +0100

    Modification scriptImport

--- scripts/scriptRefreshServerImages.sh
+++ scripts/scriptRefreshServerImages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /var/www/ca600820cc/
-#cd /Users/hurtauda/GCLChallenge/GCLChallenge
+#cd /var/www/ca600820cc/
+cd /Users/hurtauda/GCLChallenge/GCLChallenge
 echo "[]" > desktop_images.json
 echo "[]" > mobile_images.json
 
@@ -8,16 +8,24 @@ rm img/d/serv/*
 rm img/m/serv/*
 
 
-for file in /var/www/gclcimages/*; do
-#for file in /Users/hurtauda/GCLChallenge/GCLChallenge/gclcimages/*; do
-        filename=$(basename $file)
-    	/usr/bin/convert $file -scale 900x360\> img/d/serv/$filename #upload/d$filename
-        #/usr/bin/jpegtran -copy none -optimize -progressive -outfile img/d/serv/$filename upload/d$filename
-        #rm upload/d$filename
+#for file in /var/www/gclcimages/*; do
+for file in /Users/hurtauda/GCLChallenge/GCLChallenge/gclcimages/*; do
+        filename=$(basename "$file")
+        extension="${filename##*.}"
+        filenamenoextension="${filename%.*}"
+        jpgextension=".jpg"
 
-        /usr/bin/convert $file -scale 320x128\> img/m/serv/$filename #upload/m$filename
-        #/usr/bin/jpegtran -copy none -optimize -progressive -outfile img/m/serv/$filename upload/m$filename
-        #rm upload/m$filename
+        if [[ $extension -eq "png" ]]
+        then
+            /usr/bin/mogrify -path upload -convert jpg $filename
+    	    /usr/bin/convert upload/"${filenamenoextension}"$jpgextension -scale 900x360\> img/d/serv/"${filenamenoextension}"$jpgextension
+    	    /usr/bin/convert upload/"${filenamenoextension}"$jpgextension -scale 320x128\> img/m/serv/"${filenamenoextension}"$jpgextension
+    	    rm upload/$filenamenoextension$jpgextension
+    	elif [[ $extension == "jpg" ]] || [[ $extension == "jpeg" ]]
+    	then
+    	    /usr/bin/convert $file -scale 900x360\> img/d/serv/$filename
+    	    /usr/bin/convert $file -scale 320x128\> img/m/serv/$filename
+    	fi
 done
 
 /usr/bin/php scripts/makejson.php
